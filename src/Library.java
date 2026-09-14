cba7c28d5e22c74401495471aee9e76c85d5413f
Rasmus362 <Milthers.rasmus@gmail.com>
2026-09-15 01:09:31 +0200

    7 opgave lån 14/09/2026

---
+++ src/Library.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Library {
+
+    //"Opret en privat variabel ved navn books, som er en liste af Book-objekter, og giv den en ny tom ArrayList."
+    private ArrayList<Book> books = new ArrayList<>();
+    private ArrayList<Member> members = new ArrayList<>();
+    private ArrayList<Loan> loans = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void addMember(Member member) {
+        members.add(member);
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public ArrayList<Member> getMembers() {
+        return members;
+    }
+
+    public ArrayList<Loan> getLoans() {
+        return loans;
+    }
+
+    //"For hver Book i books: kald den aktuelle bog book, og udfør koden."
+    public void printBooks() {
+        for (Book book : books) {
+            IO.println(book);
+        }
+    }
+
+    public void printMembers() {
+        for (Member member : members) {
+            IO.println(member);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.time.LocalDate;
 class main {
 
     void main() {
+
+        Library library = new Library();
         Book book1 = new Book("Allan B. Downey",
                 "Think Java",
                 "9781492072508",
@@ -20,13 +22,27 @@ class main {
 
         Member member1 = new Member("Thorkild Hansen", 111);
         Member member2 = new Member("Lise Andersen", 222);
+        Member member3 = new Member("Anders Pedersen",333);
 
         Loan loan1 = new Loan(book1, member1, LocalDate.of(2026, 9, 13));
 
-        IO.println(loan1);
+        Loan loan2 = new Loan(book2, member2, LocalDate.of(2026, 9, 14));
 
+        //IO.println(loan1);
+        //IO.println("\n"+loan2);
 
+        library.addMember(member1);
+        library.addMember(member2);
+        library.addMember(member3);
 
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        IO.println("\n-----Bøger-----");
+        library.printBooks();
+        IO.println("");
+        IO.println("-----Medlemmer-----");
+        library.printMembers();
 
 
     }
